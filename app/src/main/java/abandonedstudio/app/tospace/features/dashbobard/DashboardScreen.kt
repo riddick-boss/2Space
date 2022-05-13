74c428060c1505f2d4b72c26fa816d549ddd2926
Pawel Kremienowski <kremienowski33@gmail.com>
2022-05-13 21:38:38 +0200

    weather screen setup

--- app/src/main/java/abandonedstudio/app/tospace/features/dashbobard/DashboardScreen.kt
+++ app/src/main/java/abandonedstudio/app/tospace/features/dashbobard/DashboardScreen.kt
@@ -1,6 +1,16 @@
 package abandonedstudio.app.tospace.features.dashbobard
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -8,4 +18,32 @@ fun DashboardScreen(
     viewModel: DashboardViewModel = hiltViewModel()
 ) {
 
+    val capeCanaveralWeather by viewModel.capeCanaveralWeather.collectAsState()
+    val starBaseWeather by viewModel.starBaseWeather.collectAsState()
+
+    val showWeatherLoading by viewModel.showWeatherLoading.collectAsState()
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        item("weather_card") {
+            Card(
+                modifier = Modifier.animateContentSize(),
+                elevation = 0.dp,
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                if (!showWeatherLoading) {
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        Facility.CAPE_CANAVERAL.Content(weather = capeCanaveralWeather!!)
+//                        TODO: starbase
+//                        TODO: vanden
+                    }
+                } else {
+                    CircularProgressIndicator(modifier = Modifier.size(56.dp))
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/abandonedstudio/app/tospace/features/dashbobard/DashboardViewModel.kt
+++ app/src/main/java/abandonedstudio/app/tospace/features/dashbobard/DashboardViewModel.kt
@@ -1,15 +1,48 @@
 package abandonedstudio.app.tospace.features.dashbobard
 
+import abandonedstudio.app.tospace.core.domain.util.extension.showToast
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
 class DashboardViewModel @Inject constructor(
     application: Application,
     private val dataSource: DataSource
-): AndroidViewModel(application) {
+) : AndroidViewModel(application) {
+
+    val capeCanaveralWeather: StateFlow<FacilityWeather?> by lazy {
+        flow {
+            try {
+                emit(dataSource.loadCanaveralWeather())
+            } catch (e: Exception) {
+                e.message?.run {
+                    showToast(this)
+                }
+            }
+        }
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
+    }
+
+    val starBaseWeather: StateFlow<FacilityWeather?> by lazy {
+        flow {
+            try {
+                emit(dataSource.loadCanaveralWeather()) //TODO: starbase
+            } catch (e: Exception) {
+                e.message?.run {
+                    showToast(this)
+                }
+            }
+        }
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
+    }
 
 
+    val showWeatherLoading: StateFlow<Boolean> by lazy {
+        combine(capeCanaveralWeather, starBaseWeather) { t1, t2 -> t1 != null && t2 != null }
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), true)
+    }
 }
